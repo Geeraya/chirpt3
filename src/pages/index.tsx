277038b2fd0bbbb04dc297f93d54dea245536746
Mantas Imbrasas <mantasimbrasas@gmail.com>
2023-03-26 20:13:32 +0300

    loading states

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,6 +8,7 @@ import type { RouterOutputs } from "~/utils/api";
 
 import relativeTime from "dayjs/plugin/relativeTime";
 import dayjs from "dayjs";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 dayjs.extend(relativeTime);
 
 function CreatePostWizard() {
@@ -56,12 +57,29 @@ function PostView(props: PostWithAuthor) {
   );
 }
 
-const Home: NextPage = () => {
-  const user = useUser();
-  const { data, isLoading } = api.posts.getAll.useQuery();
+function Feed() {
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
-  if (isLoading) return <div>Loading...</div>;
+  if (postsLoading) return <LoadingPage />;
+
   if (!data) return <div>Something went wrong</div>;
+
+  return (
+    <div className="flex flex-col">
+      {[...data, ...data].map((fullPost) => (
+        <PostView {...fullPost} key={fullPost.post.id} />
+      ))}
+    </div>
+  );
+}
+
+const Home: NextPage = () => {
+  const { user, isLoaded: userLoaded, isSignedIn } = useUser();
+
+  // Start fetching ASAP
+  api.posts.getAll.useQuery();
+
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -74,15 +92,11 @@ const Home: NextPage = () => {
         <div className="w-full border-x border-slate-400 md:max-w-2xl">
           <div className="flex border-b border-slate-400 p-4">
             <div className="flex justify-center">
-              {!user.isSignedIn && <SignInButton />}
-              {!!user.isSignedIn && <CreatePostWizard />}
+              {!isSignedIn && <SignInButton />}
+              {!!isSignedIn && <CreatePostWizard />}
             </div>
           </div>
-          <div className="flex flex-col">
-            {[...data, ...data].map((fullPost) => (
-              <PostView {...fullPost} key={fullPost.post.id} />
-            ))}
-          </div>
+          <Feed />
         </div>
       </main>
     </>
